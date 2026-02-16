023026395afba799c85fa4c7a759fa57461e86d5
bbeck2417 <billys.beck@gmail.com>
2026-02-16 10:47:54 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// make an event listener... it will trigger when the dom is loaded
+
+addEventListener("DOMContentLoaded", async () => {
+  const response = await fetch("http://localhost:3000/api/songs");
+  const songs = await response.json();
+
+  let html = ``;
+  for (let song of songs) {
+    html += `<li>${song.title} - ${song.artist}</li>`;
+  }
+
+  document.querySelector("#addedsongs").innerHTML = html;
+});

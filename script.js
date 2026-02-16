f2b468c18359de3735dc1bc3d94bf137f925b7b9
bbeck2417 <billys.beck@gmail.com>
2026-02-16 12:07:08 -0500

    deployed backend to replit and updated fetch url

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // make an event listener... it will trigger when the dom is loaded
 
 addEventListener("DOMContentLoaded", async () => {
-  const response = await fetch("http://localhost:3000/api/songs");
+  const response = await fetch(
+    "https://backendtutorialnodeexpress--billysbeck.replit.app/api/songs",
+  );
   const songs = await response.json();
 
   let html = ``;
